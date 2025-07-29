fbf7151e09f4af6e948b105d6f784fd3c67aef59
Swift7994 <JSwift7994@googlemail.com>
2025-07-29 14:27:34 +0100

    allowed prompt to be used as a command line arguement

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
@@ -7,9 +8,20 @@ api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
 def main():
+    if not api_key:
+        print("GEMINI_API_KEY environment variable is not set.")
+        exit(1)
+
+    if len(sys.argv) > 1:
+        contents = sys.argv[1]
+        print(f"Input: {contents}")
+    else:
+        print("No input provided.")
+        exit(1)
+
     response = client.models.generate_content(
         model='gemini-2.0-flash-001',
-        contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+        contents=contents
     )
     prompt_tokens = response.usage_metadata.prompt_token_count
     response_tokens = response.usage_metadata.candidates_token_count
