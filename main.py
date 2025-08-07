dcac8cba872aac7d5a39335a0651291115b59bf3
Swift7994 <JSwift7994@googlemail.com>
2025-08-07 19:53:36 +0100

    added function declaration for get_files_info()

--- main.py
+++ main.py
@@ -4,21 +4,33 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 from config import system_prompt
+from functions.get_files_info import schema_get_files_info
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
 
 def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+
     if not api_key:
         print("GEMINI_API_KEY environment variable is not set.")
         exit(1)
 
-    if len(sys.argv) > 1:
-        contents = sys.argv[1]
-    else:
+    client = genai.Client(api_key=api_key)
+
+    args = sys.argv[1:]
+    verbose = "--verbose" in args
+    contents = next((arg for arg in args if not arg.startswith("--")), None)
+
+    if not contents:
         print("No input provided.")
         exit(1)
+
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,
+        ]
+)
+
 
     messages = [
         types.Content(role="user", parts=[types.Part(text=contents)]),
@@ -27,19 +39,22 @@ def main():
     response = client.models.generate_content(
         model='gemini-2.0-flash-001',
         contents=messages,
-        config=types.GenerateContentConfig(system_instruction=system_prompt),
+        config=types.GenerateContentConfig(
+            tools=[available_functions],
+            system_instruction=system_prompt),
     )
 
-    if len(sys.argv) > 2:
-        if "--verbose" in sys.argv[2:]:
-            print(f"User prompt: {contents}")
-            prompt_tokens = response.usage_metadata.prompt_token_count
-            response_tokens = response.usage_metadata.candidates_token_count
-            print(f"Prompt tokens: {prompt_tokens}")
-            print(f"Response tokens: {response_tokens}")
+    if verbose:
+        print(f"User prompt: {contents}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
             
+    if response.function_calls:
+        for function_call_part in response.function_calls:
+            print(f"Calling function: {function_call_part.name}({function_call_part.args})")
             
-    print(response.text)
+    if response.text:
+        print(response.text)
 
 if __name__ == "__main__":
     main()
